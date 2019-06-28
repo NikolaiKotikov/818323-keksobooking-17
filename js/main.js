f88381495296c868a3a0448f2e44dc6fda5bbe31
Николай <nikolai.kotikov@gmail.com>
2019-06-28 22:50:47 +0300

    Задание 8

--- js/main.js
+++ js/main.js
@@ -152,6 +152,7 @@ var activatePage = function () {
   changeAccessibility(adForm, false);
   changeAccessibility(mapFilters, false);
   renderMapPin(generatedData);
+  changePrice();
   map.classList.remove('map--faded');
   adForm.classList.remove('ad-form--disabled');
 };
@@ -175,5 +176,49 @@ mapPinMain.addEventListener('click', function () {
 });
 
 var needlePointСoordinates = getNeedlePointСoordinates(MAIN_PIN_START_X, MAIN_PIN_START_Y);
-address.value = needlePointСoordinates[0] + ', ' + needlePointСoordinates[1];
+address.value = Math.round(needlePointСoordinates[0]) + ', ' + needlePointСoordinates[1];
 
+// Задание 8
+var type = document.querySelector('#type');
+var price = document.querySelector('#price');
+var timeIn = document.querySelector('#timein');
+var timeOut = document.querySelector('#timeout');
+
+/**
+ * Синхронизирует поля firstSelect и secondSelect:
+ * при изменении значения поля firstSelect
+ * в поле secondSelect выделяется соответствующее ему значение
+ * @param {*} firstSelect DOM элемент, поле select
+ * @param {*} secondSelect DOM элемент, поле select
+ */
+var synchronizeSelects = function (firstSelect, secondSelect) {
+  var currentVal = firstSelect.value;
+  for (var i = 0; i < secondSelect.children.length; i++) {
+    if (currentVal === secondSelect.children[i].value) {
+      secondSelect.children[i].selected = true;
+    }
+  }
+};
+
+/**
+ * связывает поля «Тип жилья» и «Цена за ночь»:
+ * при изменении значения в поле «Тип жилья»,
+ * меняются значения атрибутов 'min' и 'placeholder';
+ * зависимость значений хранится в 'PRICES'
+ */
+var changePrice = function () {
+  price.setAttribute('min', PRICES[type.value]);
+  price.setAttribute('placeholder', PRICES[type.value]);
+};
+
+timeIn.addEventListener('change', function () {
+  synchronizeSelects(timeIn, timeOut);
+});
+
+timeOut.addEventListener('change', function () {
+  synchronizeSelects(timeOut, timeIn);
+});
+
+type.addEventListener('change', function () {
+  changePrice();
+});
